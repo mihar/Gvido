772a760a6295451135512c4d2ec8830ce55606ee
Miha Rebernik <rasmajx@gmail.com>
2011-02-23 18:07:20 +0100

    Fixing photos.

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,6 +1,6 @@
 class Schedule < ActiveRecord::Base
   has_attached_file :file,
-                    :url  => "/assets/schedules/:id/:basename.:extension",
+                    :url  => "/system/schedules/:id/:basename.:extension",
                     :path => ":rails_root/public/system/schedules/:id/:basename.:extension"
   validates_attachment_presence :file
   validates_presence_of :title
