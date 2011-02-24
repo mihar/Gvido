1d9ce6573887f21be2406931e1aa74b9414309f2
Ljubomir Marković <ljm@disru.pt>
2011-02-24 15:47:47 -0800

    Added with_menthors scope

--- app/models/gig.rb
+++ app/models/gig.rb
@@ -6,18 +6,10 @@ class Gig < ActiveRecord::Base
   
   scope :upcoming, where('`when` > NOW()').order('`when`')
   scope :recent, where('`when` > ?', 3.month.ago).order('`when`')
-  scope :with_mentors do
-    Gig.all.map do |m|
-      m unless m.mentors.empty?
-    end.flatten.uniq.delete_if { |a| a.nil? }
-  #  gigs_with_mentors = []
-  #  for gig in all_gigs
-  #    if !gig.mentors.empty?
-  #      gigs_with_mentors << gig
-  #    end
-  #  end
-  #  gigs_with_mentors
-  end 
+  
+  def self.with_mentors
+    Gig.all.reject { |g| g.mentors.empty? }
+  end
   
   def upcoming?
     self.when > Time.now
