de5bad3ff21da7a1f6a5d7c92df8913eb902a42f
Miha Rebernik <rasmajx@gmail.com>
2011-02-25 22:01:50 +0100

    Losing the deprecated time formats.

--- config/initializers/date_formats.rb
+++
@@ -1,2 +0,0 @@
-#ActiveSupport::CoreExtensions::Date::Conversions::DATE_FORMATS.merge!(:simple => '%d. %m. %Y')
-#ActiveSupport::CoreExtensions::Time::Conversions::DATE_FORMATS.merge!(:simple => '%d. %m. %Y ob %R')
