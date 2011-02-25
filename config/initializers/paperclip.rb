56444ffd89c7b461eec0b927bb2c0d994ef4a130
Miha Rebernik <rasmajx@gmail.com>
2011-02-25 23:52:25 +0100

    Modded paperclip initializer to accept a config file.

--- config/initializers/paperclip.rb
+++ config/initializers/paperclip.rb
@@ -1,1 +1,6 @@
-Paperclip.options[:command_path] = '/usr/bin'
+paperclip_settings_file = File.join(Rails.root, "config", "paperclip.yml")
+
+if File.exists?(paperclip_settings_file)
+  paperclip_settings = YAML.load_file(paperclip_settings_file)
+  Paperclip.options[:command_path] = paperclip_settings["path_to_imagemagick"]
+end
