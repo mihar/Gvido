9c8b9912c85d9a5068159e2b0989c705049f314d
Miha Rebernik <rasmajx@gmail.com>
2011-02-25 22:06:56 +0100

    Fixing Regexp error.

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -8,7 +8,7 @@ class Movie < ActiveRecord::Base
   private
   
   def extract_youtube_id
-    yt_id = self.youtube.match /http:\/\/www\.youtube\.com\/watch\?v\=([^\"][a-zA-Z0-9-_]+)/
+    yt_id = self.youtube.match /http:\/\/www\.youtube\.com\/watch\?v\=([^\"][a-zA-Z0-9\-_]+)/
     if Regexp.last_match(1)
       self.youtube = Regexp.last_match(1)
     end
