b7efe604a7582f010e3bd1459624503eec9bdf54
Ljubomir Marković <ljm@disru.pt>
2011-02-27 09:14:12 -0800

    movies mod

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,24 @@
 class MoviesController < InheritedResources::Base
   before_filter :set_section
-  respond_to :html
+  layout :pick_layout
   
   def create
-    create! { albums_path }
+    create! { all_movies_path }
   end
   
   def update
-    update! { albums_path }
+    update! { all_movies_path }
   end
   
   def destroy
-    destroy! { albums_path }
+    destroy! { all_movies_path }
   end
-
+  
   private
+  
+  def pick_layout
+    [:index].include?(action_name.to_sym) ? "application" : "dashboard"
+  end
   
   def set_section
     @section = :albums
