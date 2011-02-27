9bab62d76a5da4f3d66533e8428f7840ab0f2856
Ljubomir Marković <ljm@disru.pt>
2011-02-27 09:09:34 -0800

    instruments controller mod

--- app/controllers/instruments_controller.rb
+++ app/controllers/instruments_controller.rb
@@ -1,11 +1,28 @@
 class InstrumentsController < InheritedResources::Base
   before_filter :set_section
-  respond_to :html
+  
+  layout :pick_layout
+  
+  def update
+    update! { all_instruments_path }
+  end
+  
+  def create
+    create! { all_instruments_path }
+  end
+  
+  def destroy
+    destroy! { all_instruments_path }
+  end
   
   protected
   
   def resource
     @instrument ||= end_of_association_chain.find_by_permalink params[:id]
+  end
+  
+  def pick_layout
+    [:index, :show].include?(action_name.to_sym) ? "application" : "dashboard"
   end
   
   def set_section
