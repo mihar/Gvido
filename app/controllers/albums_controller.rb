62cb438e9aedc5858b3f5dd24f555c47fd1ed5ea
Ljubomir Marković <ljm@disru.pt>
2011-02-27 09:08:02 -0800

    album controller needs work

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,15 +1,27 @@
 class AlbumsController < InheritedResources::Base
   before_filter :set_section
-  respond_to :html
-  
+  layout :pick_layout
+   
   def index
     @albums_by_category = AlbumCategory.includes('albums')
     @albums_no_category = Album.not_categorized
     @movies = Movie.all
-    index! #super
+    index!
+  end
+  
+  def create
+    create! { all_albums_path }
+  end
+  
+  def update
+    update! { all_albums_path }
   end
   
   private
+    
+  def pick_layout
+    [:index].include?(action_name.to_sym) ? "application" : "dashboard"
+  end
   
   def set_section
     @section = :albums
