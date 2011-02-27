a65df50e6bf9c7903ba36f8883902176d820eb98
Ljubomir Marković <ljm@disru.pt>
2011-02-27 09:15:21 -0800

    posts mod

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,13 +1,29 @@
 class PostsController < InheritedResources::Base
   before_filter :set_section  
-  respond_to :html
+  layout :pick_layout
    
   def index
     @notices = Notice.all
     super
   end
   
-  protected
+  def create
+    create! { all_posts_path }
+  end
+  
+  def update
+    update! { all_posts_path }
+  end
+  
+  def destroy
+    destroy! { all_posts_path }
+  end
+  
+  private
+  
+  def pick_layout
+    [:index].include?(action_name.to_sym) ? "application" : "dashboard"
+  end
   
   def collection
     @posts ||= end_of_association_chain.paginate :page => params[:page], :per_page => 10
