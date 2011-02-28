3614d7b1f6fefb30b01fe6a02691679030969278
Ljubomir Marković <ljm@disru.pt>
2011-02-28 02:04:47 -0800

    shop advices mod

--- app/controllers/shop_advices_controller.rb
+++ app/controllers/shop_advices_controller.rb
@@ -1,21 +1,27 @@
 class ShopAdvicesController < InheritedResources::Base
   load_and_authorize_resource
+  skip_load_resource :only => :all
   belongs_to :instrument, :finder => :find_by_permalink!
   respond_to :html
+  layout :pick_layout
   
   def create
-    create! { instrument_shop_advices_path( parent ) }
+    create! { all_instrument_shop_advices_path( parent ) }
   end
   
   def update
-    update! { instrument_shop_advices_path( parent ) }
+    update! { all_instrument_shop_advices_path( parent ) }
   end
   
   def destroy
-    destroy! { instrument_shop_advices_path( parent ) }
+    destroy! { all_instrument_shop_advices_path( parent ) }
   end
   
   private
+  
+  def pick_layout
+    [:index].include?(action_name.to_sym) ? "application" : "dashboard"
+  end
   
   def set_section
     @section = :instruments
