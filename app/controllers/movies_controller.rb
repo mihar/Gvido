20b7ae597eb7a891f9b0a7ce2bb77a14e10b2490
Ljubomir Marković <ljm@disru.pt>
2011-02-28 02:03:54 -0800

    Movies mod

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -19,7 +19,7 @@ class MoviesController < InheritedResources::Base
   private
   
   def pick_layout
-    [:index].include?(action_name.to_sym) ? "application" : "dashboard"
+    [:index, :show].include?(action_name.to_sym) ? "application" : "dashboard"
   end
   
   def set_section
