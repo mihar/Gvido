b7788724150c96ff4de49f132e02beb1a84c2d42
Ljubomir Marković <ljm@disru.pt>
2011-02-28 13:16:31 -0800

    long forgotten :new_report add to skip_load_rescue array in contacts controller

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -1,7 +1,7 @@
 class ContactsController < InheritedResources::Base
   load_and_authorize_resource
   before_filter :set_section
-  skip_load_resource :only => :all
+  skip_load_resource :only => [:all, :new_report]
   respond_to :html
 
   # Different layouts here.
