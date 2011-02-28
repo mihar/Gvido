e6bfbbaf3e08bfed9881991984208966c82f933f
Ljubomir Marković <ljm@disru.pt>
2011-02-28 02:04:14 -0800

    Photos mod

--- app/controllers/photos_controller.rb
+++ app/controllers/photos_controller.rb
@@ -1,21 +1,27 @@
 class PhotosController < InheritedResources::Base
   load_and_authorize_resource
+  skip_load_resource :only => :all
   belongs_to :album
   before_filter :set_section
+  layout :pick_layout
   
   def create
-    create! { album_path(parent) }
+    create! { all_album_photos_path(parent) } 
   end
   
   def update
-    update! { album_path(parent) }
+    update! { all_album_photos_path(parent) }
   end
   
   def destroy
-    destroy! { album_path(parent) }
+    destroy! { all_album_photos_path(parent) }
   end
   
   private
+  
+  def pick_layout
+    [:index, :show].include?(action_name.to_sym) ? "application" : "dashboard"
+  end
   
   def set_section
     @section = :albums
