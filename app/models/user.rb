cf1aa00bb60555da541b00e7a54308cafd2e004b
Ljubomir Marković <ljm@disru.pt>
2011-04-02 20:31:14 -0700

    Added new associations for user and a method for checking mentors role

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
+  has_one :mentor
+  has_many :lessons
+  
   # Include default devise modules. Others available are:
   # :token_authenticatable, :confirmable, :lockable and :timeoutable
   devise :database_authenticatable, :rememberable, :trackable, :validatable
@@ -7,6 +10,10 @@ class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name, :admin, :email, :password, :password_confirmation, :remember_me
   
   validates_presence_of :first_name, :last_name
+  
+  def mentor?
+    not mentor.nil?
+  end
   
   def full_name
     "#{first_name} #{last_name}"
