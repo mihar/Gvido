33bcfda596088b80058cfb7bec3a76fcf17491db
Ljubomir Marković <ljm@disru.pt>
2011-04-04 11:52:04 -0700

    mentors full name display in a gig

--- app/helpers/gigs_helper.rb
+++ app/helpers/gigs_helper.rb
@@ -1,7 +1,7 @@
 module GigsHelper
   def preforming_mentors(gig)
     unless gig.mentors.empty?
-      return raw(gig.mentors.map { |m| m.name }.join(', '))
+      return raw(gig.mentors.map { |m| m.full_name }.join(', '))
     end
     return 'Brez mentorjev'
   end
