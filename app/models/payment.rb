7bcba9788eda9a5bb9655bcbf5646fda703a7d40
Miha Rebernik <rasmajx@gmail.com>
2011-04-07 11:52:22 +0200

    Refactoring Payment model scopes.

--- app/models/payment.rb
+++ app/models/payment.rb
@@ -2,24 +2,13 @@ class Payment < ActiveRecord::Base
   belongs_to  :enrollment
   has_one     :payment_exception
   has_one     :lesson, :dependent => :destroy
- 
-  def self.settled_payments(enrollment_id)
-    where("enrollment_id = ? AND settled = ?", enrollment_id, true)
-  end
   
-  def self.unsettled_payments(enrollment_id)
-    where("enrollment_id = ? AND settled = ?", enrollment_id, false)
-  end
+  scope :settled, where(:settled => true)
+  scope :unsettled, where(:settled => false)
+  scope :due_this_month, where(:payment_date => Date.today.beginning_of_month..Date.today.end_of_month)
+  scope :due, where("payment_date < ?", 1.day.from_now)
   
-  def self.settled_payment_dates_for_enrollment(enrollment_id)
-    settled_payments(enrollment_id).map &:payment_date
-  end
-  
-  def self.destroy_unsettled_payments_for_enrollment(enrollment_id)
-    unsettled_payments(enrollment_id).destroy_all
-  end
-  
-  #returns a collection of all payment dates in hashes (keys are years, values are payable months)
+  # Returns a collection of all payment dates in hashes (keys are years, values are payable months)
   def self.all_payment_dates
     (self.find_by_sql("SELECT DISTINCT payment_date FROM payments ORDER BY payment_date ASC").map(&:payment_date)).group_by {|p| p.year}
   end
