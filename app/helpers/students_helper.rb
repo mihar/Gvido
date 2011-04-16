74e39954cd84981f55f0393d39bff28765a97b56
Ljubomir Markovic <ljm@disru.pt>
2011-04-16 16:22:57 +0200

    Changed display info, added payment_type view helper

--- app/helpers/students_helper.rb
+++ app/helpers/students_helper.rb
@@ -2,5 +2,15 @@ module StudentsHelper
   def net_ammount(total_price, discount)
     discount > 0 ? total_price - total_price * discount : total_price
   end
+  
+  def payment_type(value)
+    case value
+      when 1 then return "Navadno plačilo"
+      when 2 then return "Odšteta polovica kavcije"
+      when 3 then return "Odšteta celotna kavcija"
+      when 4 then return "Kavcija"
+      when 5 then return "Vpisnina"
+    end
+  end
 end
 
