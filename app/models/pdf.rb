f6ee4ec4681c2619c1b367b0c3a177eda23838e0
Ljubomir Markovic <ljm@disru.pt>
2011-06-16 00:58:07 +0200

    Fixed basic pdf views for invoices

--- app/models/pdf.rb
+++ app/models/pdf.rb
@@ -6,6 +6,63 @@ class Pdf
   NORMAL_FONT_SIZE = 10
   FONT = "#{Prawn::BASEDIR}/data/fonts/DejaVuSans.ttf"
   
+  module Invoice
+    class << self
+      # top padding = 4.5cm == 127.55pt
+      # left padding => 2cm == 56.69
+      # box size => 9cm x 4.5cm == 255.11pt x 127.55pt
+      def address_box(pdf, name, address)
+        pdf.bounding_box  [0, pdf.bounds.absolute_top - 127.55], :width => 255.11, :height => 127.55 do
+          pdf.move_down 5
+          pdf.text name
+          pdf.move_down 5
+          pdf.text address
+        end
+        pdf  
+      end
+
+      def logo_box(pdf)
+        pdf.bounding_box  [350, pdf.bounds.absolute_top - 127.55], :width => 255.11, :height => 127.55 do
+          pdf.move_down 5
+          pdf.text "Glasbena šola gvido"
+        end
+        pdf
+      end
+
+      def small_invoice_box(pdf, payment_date, payers_name, payers_address, recievers_name, recievers_address, monthly_reference)
+        pdf.bounding_box  [10, pdf.cursor - 10], :width => 200, :height => 245.11 do
+          pdf.text payers_name
+          pdf.text payers_address
+          pdf.text payment_date
+          pdf.text monthly_reference
+          pdf.text recievers_name
+          pdf.text recievers_address
+        end
+        pdf
+      end
+
+      def big_invoice_box(pdf, payment_date, payers_name, payers_address, recievers_name, recievers_address, monthly_reference)
+        pdf.bounding_box  [220, pdf.cursor + 245.11], :width => 200, :height => 245.11 do
+          pdf.text payers_name
+          pdf.text payers_address
+          pdf.text payment_date
+          pdf.text monthly_reference
+          pdf.text recievers_name
+          pdf.text recievers_address
+        end
+        pdf
+      end
+
+      def invoice_box(pdf, payment_date, payers_name, payers_address, recievers_name, recievers_address, monthly_reference)
+        pdf.bounding_box  [0, pdf.bounds.absolute_bottom + 255], :width => 560, :height => 255 do
+          pdf = small_invoice_box(pdf, payment_date, payers_name, payers_address, recievers_name, recievers_address, monthly_reference)
+          pdf = big_invoice_box(pdf, payment_date, payers_name, payers_address, recievers_name, recievers_address, monthly_reference)
+          pdf.stroke_bounds
+        end
+        pdf
+      end
+    end
+  end
   class << self
     def add_page_numbers(pdf)
       pdf.number_pages "Stran <page> od <total>", [pdf.bounds.right - 80, -20]
--- app/views/invoices/index.pdf.prawn
+++ app/views/invoices/index.pdf.prawn
@@ -1,11 +1,9 @@
 pdf = Pdf.style(pdf)
 
 for invoice in @invoices
-  pdf.text invoice.student.to_s
-  pdf.text invoice.payers_address
-  pdf.text invoice.monthly_reference
-  pdf.text I18n.l invoice.payment_date
-  pdf.text number_to_currency(invoice.price)
+  pdf = Pdf::Invoice.address_box(pdf, invoice.student.to_s, invoice.payers_address)
+  pdf = Pdf::Invoice.logo_box(pdf)
+  pdf = Pdf::Invoice.invoice_box(pdf, I18n.l(invoice.payment_date), invoice.payers_name, invoice.payers_address, invoice.recievers_name, invoice.recievers_address, invoice.monthly_reference)
   
   pdf.start_new_page if invoice != @invoices.last
 end
