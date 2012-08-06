218ee35366d29343659a7e8a8b410f897b927271
Miha Rebernik <rasmajx@gmail.com>
2012-08-06 16:51:20 +0200

    Assets on S3.

--- app/models/notice.rb
+++ app/models/notice.rb
@@ -4,8 +4,14 @@ class Notice < ActiveRecord::Base
   validates_presence_of :title, :body
   has_attached_file :photo, 
                     :styles => { :square => "75x75#", :small => "150x150#", :medium => "180x180#", :normal => "550x550>" },
-                    :url  => "/system/notices/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/system/notices/:id/:style/:basename.:extension"
+                    :whiny => false,
+                    :storage => :s3,
+                    :bucket => AWS_S3['bucket'],
+                    :s3_credentials => {
+                      :access_key_id => AWS_S3['access_key_id'],
+                      :secret_access_key => AWS_S3['secret_access_key']
+                    },
+                    :path => '/notices/:id/:style/:basename.:extension'
                 
   
   
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,9 +1,15 @@
 class Photo < ActiveRecord::Base
   belongs_to :album
-  has_attached_file :photo, 
-                    :styles => { :mini => "50x50#", :square => "75x75#", :small => "150x150#", :medium => "180x180#", :normal => "550x550>" },
-                    :url  => "/system/photos/:id/:style/:basename.:extension",
-                    :path => ":rails_root/public/system/photos/:id/:style/:basename.:extension"
+
+  has_attached_file :photo, :styles => { :mini => "50x50#", :square => "75x75#", :small => "150x150#", :medium => "180x180#", :normal => "550x550>" },
+                    :whiny => false,
+                    :storage => :s3,
+                    :bucket => AWS_S3['bucket'],
+                    :s3_credentials => {
+                      :access_key_id => AWS_S3['access_key_id'],
+                      :secret_access_key => AWS_S3['secret_access_key']
+                    },
+                    :path => '/photos/:id/:style/:basename.:extension'
   
   validates_attachment_presence :photo
 end
