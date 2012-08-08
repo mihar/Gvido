83f9f9fffc8d2e6e4f9538e517fafcdfd1a0b4bb
Miha Rebernik <rasmajx@gmail.com>
2012-08-08 12:41:53 +0200

    Banners -> AWS

--- app/models/banner.rb
+++ app/models/banner.rb
@@ -1,4 +1,12 @@
 class Banner < ActiveRecord::Base
   attr_accessible :url, :image
-  has_attached_file :image
+  has_attached_file :image,
+                    :whiny => false,
+                    :storage => :s3,
+                    :bucket => AWS_S3['bucket'],
+                    :s3_credentials => {
+                      :access_key_id => AWS_S3['access_key_id'],
+                      :secret_access_key => AWS_S3['secret_access_key']
+                    },
+                    :path => '/assets/banners/:id/:style/:basename.:extension'
 end
