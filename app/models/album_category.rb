1038c9018fec4f00b3b7fa41e0505c0e9c17cb39
Miha Rebernik <rasmajx@gmail.com>
2012-08-25 19:56:50 -0700

    Child deletion and warnings.

--- app/models/album_category.rb
+++ app/models/album_category.rb
@@ -1,4 +1,4 @@
 class AlbumCategory < ActiveRecord::Base
   default_scope order(:position)
-  has_many :albums
+  has_many :albums, :dependent => :destroy
 end
--- app/models/link_category.rb
+++ app/models/link_category.rb
@@ -1,5 +1,5 @@
 class LinkCategory < ActiveRecord::Base
-  has_many :links, :foreign_key => "category_id"
+  has_many :links, :foreign_key => "category_id", :dependent => :nullify
 
   def to_s
     title
