e9ffe80cea4146545ef7ebf659aa6a2ffef19b44
Miha Rebernik <miha@rebernik.info>
2013-08-28 12:54:06 +0200

    Fixing table row links and consistency.

--- app/assets/javascripts/table_row_links.js
+++ app/assets/javascripts/table_row_links.js
@@ -1,5 +1,7 @@
 $(function() {
   $("tr.clickable").click(function(event) {
+		if (event.target && event.target.nodeName === 'A')
+			return
     var link = $(this).find("a")
     if ( link )
       window.location = link.attr("href")
